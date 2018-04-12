5c9544baf80058391ebf3d8b768bdb204b4802e1
Patrick Kunka <patrick@kunkalabs.com>
2018-04-12 22:52:16 +0100

    increase insertLineBreak coverage to 100%

--- src/State/Edits/insertLineBreak.test.ts
+++ src/State/Edits/insertLineBreak.test.ts
@@ -8,6 +8,21 @@ import insertLineBreak from './insertLineBreak';
 const assert = chai.assert;
 
 describe('insertLineBreak()', () => {
+    it('should insert a block break at the selection', () => {
+        const prevState = Object.assign(new State(), {
+            text: 'Line one.',
+            markups: [
+                new Markup([MarkupTag.P, 0, 9])
+            ]
+        });
+
+        const nextState = insertLineBreak(prevState, {from: 4, to: 4});
+
+        assert.equal(nextState.text, 'Line\n one.');
+        assert.equal(nextState.selection.from, 5);
+        assert.equal(nextState.selection.to, 5);
+    });
+
     it('should coerce consecutive line breaks into a block break from behind', () => {
         const prevState = Object.assign(new State(), {
             text: 'Line o\nne.',
